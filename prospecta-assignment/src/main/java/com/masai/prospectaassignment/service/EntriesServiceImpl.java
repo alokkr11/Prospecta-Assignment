3bf8088c582e56ff681cab3d01febf5d21f36a07
Alok Kumar <alokk0018@gmail.com>
2023-05-03 14:25:39 +0530

    error fixed

--- prospecta-assignment/src/main/java/com/masai/prospectaassignment/service/EntriesServiceImpl.java
+++ prospecta-assignment/src/main/java/com/masai/prospectaassignment/service/EntriesServiceImpl.java
@@ -3,10 +3,12 @@ package com.masai.prospectaassignment.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.masai.prospectaassignment.model.Entries;
 import com.masai.prospectaassignment.repository.EntriesRepository;
 
+@Service
 public class EntriesServiceImpl implements EntriesService {
 
 	@Autowired
